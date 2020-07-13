330043c904183b23491e779cf3da191b5ff796f3
Antonio James <asjames18@gmail.com>
2020-07-13 19:47:54 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(string)
 
-  original_array = string.split(" ")
+  array = string.split(" ")
   return_array = []
-  original_array.each do|string|
+  array.each do|string|
     return_array << string.reverse
   end
   return_array.join(" ")
@@ -10,11 +10,11 @@ end
 
 def reverse_each_word(string)
   array = string.split(" ")
-  test_array = []
-  array.collect do|string| 
-    test_array << string.reverse
+  return_array = []
+  array.collect do|string|
+    return_array << string.reverse
   end
-  test_array.join(" ")
+  return_array.join(" ")
 end
 
 
